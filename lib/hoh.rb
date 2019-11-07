a51648d445ef6f0d1ab8ac963f6d9456168db8d1
Alejandro Linan <jandrolinan@gmail.com>
2019-11-07 16:21:01 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,6 +43,25 @@ def naming_system
   # HoH all the way to Species!
   hashes = {
     :label => "Kingdom",
-    :sub_category =>
+    :sub_category => {
+      :label "Phylum",
+      :sub_category => {
+        :label => "Class",
+        :sub_category => {
+          :label => "Order",
+          :sub_category => {
+            :label => "Family",
+            :sub_category => {
+              :label => "Genus",
+              :sub_category => {
+                :label => "Species",
+                :sub_category => nil 
+              }
+            }
+          }
+        }
+      }
+    }
   }
+  return hases
 end
